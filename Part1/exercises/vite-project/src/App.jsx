b36c6bc58148bbc33bfb48e79d16778acd458d5b
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-02-16 02:46:33 -0800

    Update Part1 Ex1.4

--- Part1/exercises/vite-project/src/App.jsx
+++ Part1/exercises/vite-project/src/App.jsx
@@ -5,18 +5,20 @@ import './App.css'
 const App = () => {
   //Exercise 1.3
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   /*
   Header takes care of rendering the name of the course, 
@@ -33,37 +35,47 @@ const App = () => {
   }
   //Content
   const Content = (props) => {
-    console.log(props)
+    console.log("Content"+props)
+    console.log(parts)
     return (
       // <p>{props.name} {props.exercise}</p>
+      // <div>
+      //   <Part name={part1.name} exercises={part1.exercises} />
+      //   <Part name={part2.name} exercises={part2.exercises} />
+      //   <Part name={part3.name} exercises={part3.exercises} />
+      // </div>
       <div>
-        <Part name={part1.name} exercises={part1.exercises} />
-        <Part name={part2.name} exercises={part2.exercises} />
-        <Part name={part3.name} exercises={part3.exercises} />
+        {
+          props.parts.map(e => {
+            return <p>{e.name} {e.exercises}</p>
+          })
+        }
       </div>
     )
   }
   //Total
   const Total = (props) => {
     console.log(props)
+    let sum = 0;
+    parts.forEach(e => sum+=e.exercises)
     return (
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {sum}</p>
     )
   }
 
-  const Part = (props) => {
-    console.log(props)
-    return (
-      <p>{props.name} {props.exercises}</p>
-    )
-  }
+  //Parts
+  // const Part = (props) => {
+  //   console.log(props)
+  //   return (
+  //     <p>{props.name} {props.exercises}</p>
+  //   )
+  // }
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
-
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
