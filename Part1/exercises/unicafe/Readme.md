b5b27524e9c5f3499cf8ac5152377de3c9c689d9
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-03-06 17:22:55 -0800

    Update Part1 Unicafe Ex1.6

---
+++ Part1/exercises/unicafe/Readme.md
@@ -0,0 +1,3 @@
+To run file:
+1. (One-time) `cd` to `unicafe` folder and run `npm install`
+2. Run `npm run dev`
---
+++ Part1/exercises/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  // let setBad = () => {setBad+=1}
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div>
+        <button className='btn btn-success' onClick={() => setGood(good+1)}>good</button>
+        <button className='btn btn-warning'onClick={() => setNeutral(neutral+1)}>neutral</button>
+        <button className='btn btn-error'onClick={() => setBad(bad+1)}>bad</button>
+      </div>
+
+      <h1>Statistics</h1>
+      <div>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
