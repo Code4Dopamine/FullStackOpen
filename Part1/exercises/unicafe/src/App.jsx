12b6129d5523ac866e28609ff15453e2412f9e86
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-03-07 16:05:57 -0800

    Update Part1 Unicafe Ex1.8

--- Part1/exercises/unicafe/src/App.jsx
+++ Part1/exercises/unicafe/src/App.jsx
@@ -2,39 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const Statistics = (props) => {
+  let all = props.good + props.neutral + props.bad
+  let average = parseFloat((props.good - props.bad) / (props.good + props.neutral + props.bad)).toFixed(4)
+  let positive = parseFloat(props.good / (props.good + props.neutral + props.bad) * 100).toFixed(2)
+
+  // console.log("all:",all)
+  // console.log("average:",average)
+  // console.log("positive:",positive)
+
+    return (
+      <>
+        <h1 className='py-5'>Statistics</h1>
+        <div>
+          <p>good: {props.good}</p>
+          <p>neutral: {props.neutral}</p>
+          <p>bad: {props.bad}</p>
+
+          <div className="divider"></div>
+
+          <p>all: {all}</p>
+          <p>average: {isNaN(average) ? 0 : average}</p>
+          <p>positive: {isNaN(positive) ? 0 : positive}</p>
+
+        </div>
+      </>
+    )
+
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  // const [all,average,positive] = 0
+  // const [average, setAverage] = useState(0)
+  // const all = 
   
-  // let setBad = () => {setBad+=1}
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <div>
-        <button className='btn btn-success' onClick={() => setGood(good+1)}>good</button>
-        <button className='btn btn-warning'onClick={() => setNeutral(neutral+1)}>neutral</button>
-        <button className='btn btn-error'onClick={() => setBad(bad+1)}>bad</button>
+      <div className='py-5'>
+        <button className='btn btn-success' onClick={() => setGood(good + 1)}>good</button>
+        <button className='btn btn-warning' onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button className='btn btn-error' onClick={() => setBad(bad + 1)}>bad</button>
       </div>
 
-      <h1>Statistics</h1>
-      <div>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-
-        <div className="divider"></div> 
-
-        <p>all: {good+neutral+bad}</p>
-        <p>average: {parseFloat((good-bad)/(good+neutral+bad)*100).toFixed(2)} %</p>
-        <p>positive: {parseFloat(good/(good+neutral+bad)*100).toFixed(2)} %</p>
-
-
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
     </div>
   )
