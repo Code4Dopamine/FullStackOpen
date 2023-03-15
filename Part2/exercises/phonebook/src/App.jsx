e8fe9e1f16b1d67be46f008ebe66528b9c7e2c59
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-03-14 22:10:58 -0700

    Update Part2 phonebook Ex2.6

---
+++ Part2/exercises/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target);
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleInput = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          <div>debug: {newName} </div>
+          name: <input value={newName} onChange={handleInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(e => {
+        return <p key={e.name}>{e.name}: #</p>
+      })}
+    </div>
+  )
+}
+
+export default App
