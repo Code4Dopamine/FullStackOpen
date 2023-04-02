c452e956504031adc3f00deb573eb319915edd20
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-04-01 23:53:59 -0700

    Updated countries

--- Part2/exercises/countries/src/App.jsx
+++ Part2/exercises/countries/src/App.jsx
@@ -6,40 +6,55 @@ import axios from 'axios'
 
 function App() {
   const [searchInput, setSearchInput] = useState('')
-  const [result, setResults] = useState({})
-  const [countries, setCountries] = useState()
+  const [countries, setCountries] = useState([])
 
   useEffect(() => {
-    console.log('Effect Run')
-    axios
-      .get(`https://restcountries.com/v3.1/all/`)
-      // .then(res => {
-        //   setCountries(res.data)
-        // })
-        .then(res => res.json())
-        .then(json => {
-          console.log(json)
-          setCountries(json)
-        })
-  },[])
+    console.log('useEffect Run')
+    axios.get(`https://restcountries.com/v3.1/all/`)
+      .then(res => {
+        console.log(res.data)
+        setCountries(res.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
 
   const handleInput = (event) => {
-    // console.log(event.target.value)
     setSearchInput(event.target.value)
   }
 
-  console.log(countries)
+  const filteredCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(searchInput.toLowerCase())
+  })
 
   return (
     <div>
-      <p>Find Counties <input type="text" value={searchInput} onChange={handleInput} /></p>
-      {/* {countries.map(e => {
-        <p>{e}</p>
-      })} */}
-      <p>{countries}</p>
-      {/* <pre>
-        {JSON.stringify(countries,null,2)}
-      </pre> */}
+      <p>Find Countries: <input type="text" value={searchInput} onChange={handleInput} /></p>
+      {filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filteredCountries.length === 1 ? (
+        <div key={filteredCountries[0].name.common}>
+          <h2>{filteredCountries[0].name.common}</h2>
+          <p>Capital: {filteredCountries[0].capital}</p>
+          <p>Area: {filteredCountries[0].area}</p>
+
+          {/* Show list of languages */}
+          <h2>Languages</h2>
+          <ul>
+            {Object.values(filteredCountries[0].languages).map((language) => (
+              <li key={language}>{language}</li>
+            ))}
+          </ul>
+
+          {/* Show Country Flag */}
+          <img style={{ maxWidth: '200px' }} src={filteredCountries[0].flags.svg} alt={`${filteredCountries[0].name.common} flag`} />
+        </div>
+      ) : (
+        filteredCountries.map((country) => (
+          <div key={country.name.common}>
+            <p>{country.name.common}</p>
+          </div>
+        ))
+      )}
     </div>
   )
 }
